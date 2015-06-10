84ac7c47466b2dbb1bf88c410f69de94e122d990
mef1st0 <garudziec@gmail.com>
2015-06-11 00:06:38 +0200

    static files

--- zstio/settings.py
+++ zstio/settings.py
@@ -113,3 +113,8 @@ LOCALE_PATHS = (
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'website/static/'),
+)
