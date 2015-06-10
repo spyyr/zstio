599f595ab733594b6355c85bd30da5d3389b8e06
mef1st0 <garudziec@gmail.com>
2015-06-11 00:10:04 +0200

    static files

--- zstio/urls.py
+++ zstio/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from website import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name="index")
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += patterns('django.views.static',
+    (r'media/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT}),
+)
